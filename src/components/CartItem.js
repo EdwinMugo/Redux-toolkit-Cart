9569fd7f9a19b9a048094db8a90f6264bbe364fc
EdwinMugo <edwinmurgoh@gmail.com>
2024-09-24 16:43:07 +0300

    first commit

---
+++ src/components/CartItem.js
@@ -0,0 +1,39 @@
+import { ChevronDown, ChevronUp } from "../icons";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
+function CartItem({id, img, title, price, amount}) {
+  const dispatch = useDispatch();
+
+  return (
+    <article className="cart-item"> 
+    <img src={img} alt={title}/>
+    <div> 
+      <h4> {title} </h4>
+      <h4 className="item-price">${price} </h4>
+      <button className="remove-btn" onClick={()=> {
+        dispatch(removeItem(id))
+      }}> remove </button>
+    </div>
+    <div>
+      <button className="amount-btn" onClick={()=>{
+        dispatch(increase ({id}))// why are we passing in the id in distructured format
+      }}>
+        <ChevronUp/>
+         </button>
+         <p className="amount">{amount}</p>
+         <button className="amount-btn" onClick={()=>{
+          if(amount === 1){
+            dispatch(removeItem(id));
+            return; //why do we need the return satement?
+          }
+        dispatch(decrease ({id}))
+      }}>
+        <ChevronDown/>
+         </button>
+    </div>
+    </article>
+  )
+}
+
+export default CartItem
